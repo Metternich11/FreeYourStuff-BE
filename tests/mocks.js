882f5af44a2a723e6d0c9861ac72d7fb5ba1d906
Freddie Smith <fs@aeonian-tech.com>
2019-04-29 18:37:57 +0200

    feat(index.test.js):Basic endpoint testing to check http responses

--- tests/mocks.js
+++ tests/mocks.js
@@ -3,14 +3,12 @@ const mocks = {
     time: '2018-08-01T10:49:32.423Z',
     tags: ['motor vehicle','moped','scooter'],
     updated: 0,
-    _id: '5b6190529b48300014ce2cfa',
     picture: 'https://res.cloudinary.com/dh9xnvxbz/image/upload/c_fill,h_480,w_820/v1533120579/zrjn7dcll8t1wqazi71m.jpg',
     location: {
         lat: 41.3968884,
         lng: 2.1989088
     },
     address: 'Carrer de Ramon Turró, 148, 08005 Barcelona, Spain',
-    __v: 0
   }
 };
 
