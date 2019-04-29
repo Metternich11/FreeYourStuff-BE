8782674d4703d718796d57dce1dcf4a1cf7c955c
Freddie Smith <fs@aeonian-tech.com>
2019-04-29 18:37:57 +0200

    feat(index.test.js):Basic endpoint testing to check http responses

--- index.test.js
+++ index.test.js
@@ -2,69 +2,70 @@ const App = require('./app');
 const app = App.create();
 const request = require('supertest');
 const mocks = require('./tests/mocks');
+const mongoose = require('mongoose');
 
+afterAll(async () => {
+  await mongoose.connection.db.dropDatabase();
+  App.close()
+});
 
-afterAll(App.close);
+describe('ROUTES', () => {
 
-describe('routes: /getStuff', () => {
-
-  test('should respond as expected', () => {
-    request(app)
+  test('/getStuff should respond with HTTP 200', async () => {
+    await request(app)
       .get('/getStuff')
       .expect(200)
-      .end((err, res) => {
-        if (err) throw err;
-        console.log(res);
+      .then(res => {
+        expect(Array.isArray(res.body)).toBe(true)
       })
-      // .expect(Array.isArray(response.body)).toBe(true)
-    // const response = await request(app).get('/getStuff');
-    // expect(response.status).toEqual(200);
-    // expect(Array.isArray(response.body)).toBe(true);
   });
 
-  // test('should respond as expected', async () => {
-  //   request(app)
-  //     .post('/create')
-  //     .send(mocks.stuff)
-  //     .expect(200)
-  //     .end((err, res) => {
-  //       if (err) throw err;
-  //       console.log(res);
-  //     })
-  //   });
-})
+  test('/create should respond with HTTP 200', async () => {
+    await request(app)
+      .post('/create')
+      .send(mocks.stuff)
+      .expect(200)
+      .then(res => {
+        expect(res.body).toMatchObject(mocks.stuff)
+      })
 
-// describe('routes: /create', () => {
-//   test('should respond as expected', async () => {
-//     request(app)
-//       .post('/create')
-//       .send(mocks.stuff)
-//       .expect(200)
-//       .end((err, res) => {
-//         if (err) throw err;
-//         console.log(res);
-//       })
-      // .expect(response => console.log('🎁: ', response))
+  });
 
-      // .expect(res.body).to.be.not.undefined;
-      // .end(function(err, res) {
-      //
-      //     done();
-      // })
+  test('/update should respond with HTTP 204', async () => {
+    let idNumber;
 
-  // });
-// });
+    await request(app)
+      .post('/create')
+      .send(mocks.stuff)
+      .expect(200)
+      .then(res => idNumber = res.body._id)
 
-// describe('routes: /update/:id', () => {
-//   test('should respond as expected', async () => {
-//     const response = await request(app).get('/update/5cc31e33abff1a0013d1b7ba');
-//     expect(response.status).toEqual(204);
-//   });
-// });
-//
-// describe('routes: /delete/:id', () => {
-//   test('should respond as expected', async () => {
-//     const response = await request(app).get('/delete');
-//     expect(response.status).toEqual(204);
-//   });
-// });
+    await request(app)
+      .put(`/update/${idNumber}`)
+      .set('Content-Type', 'application/json')
+      .expect(204)
+      // make sure we send the body in the put here.
+
+
+    // do a get to check that the update actually worked as intended.
+  });
+
+  test('/delete should respond with HTTP 204', async () => {
+    let idNumber;
+
+    await request(app)
+      .post('/create')
+      .send(mocks.stuff)
+      .expect(200)
+      .then(res => res.body._id)
+      .then(id => idNumber = id)
+
+    await request(app)
+      .delete(`/delete/${idNumber}`)
+      .set('Content-Type', 'application/json')
+      .expect(204)
+  });
+
+
+
+});
