8859d86915cba9bede236843a454fd42d5c8ddd6
Freddie Smith <fs@aeonian-tech.com>
2019-04-30 13:00:23 +0200

    feat(tests):

--- tests/mocks.js
+++ tests/mocks.js
@@ -1,15 +1,16 @@
 const mocks = {
   stuff: {
     time: '2018-08-01T10:49:32.423Z',
-    tags: ['motor vehicle','moped','scooter'],
+    tags: ['motor vehicle', 'moped', 'scooter'],
     updated: 0,
-    picture: 'https://res.cloudinary.com/dh9xnvxbz/image/upload/c_fill,h_480,w_820/v1533120579/zrjn7dcll8t1wqazi71m.jpg',
+    picture:
+      'https://res.cloudinary.com/dh9xnvxbz/image/upload/c_fill,h_480,w_820/v1533120579/zrjn7dcll8t1wqazi71m.jpg',
     location: {
-        lat: 41.3968884,
-        lng: 2.1989088
+      lat: 41.3968884,
+      lng: 2.1989088
     },
-    address: 'Carrer de Ramon Turró, 148, 08005 Barcelona, Spain',
+    address: 'Carrer de Ramon Turró, 148, 08005 Barcelona, Spain'
   }
-};
+}
 
-module.exports = mocks;
+module.exports = mocks
